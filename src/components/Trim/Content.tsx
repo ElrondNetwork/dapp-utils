b31d25b95da4f9ecc2a466852c2f3dbaa6443e70
radu <radu.buciuceanu@gmail.com>
2021-09-27 21:47:12 +0300

    [BRD-152] Create content component

---
+++ src/components/Trim/Content.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { useComponent } from 'components/Trim/hooks/useComponent';
+
+export const Content = () => {
+  const Component = useComponent()
+
+  return <Component />
+}
